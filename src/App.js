909b3cc9064f0c43c8da8c43ba5b4cc950678a42
rahmat058 <md.kazi.rahmat@gmail.com>
2018-12-29 17:47:15 +0600

    scroll fire function is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css'
 import './assets/css/main.css'
 
 import Navbar from './components/navbar'
 
 class App extends Component {
+
+  // componentDidMount() {
+  //   const options = [
+  //     {
+  //       selector: '.main-text',
+  //       offset: 0,
+  //       callback: function(el) {
+  //         Materialize.fadeInImage(el)
+  //       }
+  //     }
+  //   ]
+
+  //   Materialize.scrollFire(options)
+  // }
+  
   render() {
     return (
       <div>
